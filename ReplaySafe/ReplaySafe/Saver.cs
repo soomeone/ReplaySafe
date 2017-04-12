2e0c739d998aca55c819b0a1411e7059761626c2
soomeone <tipdf@icloud.com>
2017-04-12 22:33:37 +0200

    Added restore function
    Like files will be automatically backed up, inexistent replay files will
    now be restored too

--- ReplaySafe/ReplaySafe/Saver.cs
+++ ReplaySafe/ReplaySafe/Saver.cs
@@ -43,11 +43,11 @@ namespace ReplaySafe
             // Begin watching.
             watcher.EnableRaisingEvents = true;
 
-            Console.WriteLine("Started file listener");
+            Console.WriteLine("Started file listener on " + path);
         }
 
         // Define the event handlers.
-        private void OnCreate(object source, FileSystemEventArgs e)
+        private async void OnCreate(object source, FileSystemEventArgs e)
         {
             String filename = e.FullPath;
 
@@ -56,26 +56,32 @@ namespace ReplaySafe
                 // Backup file when new created
                 Console.WriteLine("Found new file " + e.FullPath);
 
+                await Task.Delay(1000);
                 BackupFile(e.FullPath);
             }
         }
 
         private void CopyEverything(String frompath)
         {
-            // Looks for every file in dir and copy new ones
             String[] SourceFiles = Directory.GetFiles(frompath);
             String[] BackupFiles = Directory.GetFiles(BackupPath);
 
+            // Looks for every file in dir and copy new ones
             foreach (String file in SourceFiles)
             {
                 if (IsReplayFile(file))
                     BackupFile(file);
             }
+
+            // Restore file that have been deleted
+            foreach (String file in BackupFiles)
+            {
+                RestoreFile(file);
+            }
         }
 
-        private async void BackupFile(String File)
+        private void BackupFile(String File)
         {
-            await Task.Delay(1000);
             // Copy a single file to the backup dir
             String filename = getFileName(File);
             String newpath = BackupPath + Path.DirectorySeparatorChar + filename;
@@ -87,9 +93,24 @@ namespace ReplaySafe
                 Console.WriteLine("Backuped file " + filename);
             }
             else
-                Console.WriteLine("Skipped file " + filename);
+                Console.WriteLine("Skipped backup file " + filename);
         }
 
+        private void RestoreFile(String File)
+        {
+            // Restore a single file to the osu /r dir
+            String filename = getFileName(File);
+            String newpath = SourcePath + @"\Data\r" + Path.DirectorySeparatorChar + filename;
+
+            // Check if file already exists
+            if (!System.IO.File.Exists(newpath))
+            {
+                System.IO.File.Copy(File, newpath);
+                Console.WriteLine("Restored file " + filename);
+            }
+            else
+                Console.WriteLine("Skipped restore file " + filename);
+        }
         private String getFileName(String path)
         {
             // Split file by \ and get last arg (it's the file name)
